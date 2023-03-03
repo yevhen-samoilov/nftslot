dbf4bace1ab97894757493c85872c4df4be19b3c
Mac <yevhensamoilov@icloud.com>
2023-03-04 00:28:17 +0100

    v1.1.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "6316a1ea51699476241dde349db3255f",
+  "version.json": "1a07d96790cd6f569065843f66549c94",
 "splash/img/light-2x.png": "7a9051bfb0e8869c153b02bd8ab964c0",
 "splash/img/dark-4x.png": "80d10348cc3e9a4ff0a6137c2677b678",
 "splash/img/light-3x.png": "f3ba2e9dade2e0150f0643ad65d6f1d3",
@@ -14,9 +14,9 @@ const RESOURCES = {
 "splash/img/light-1x.png": "30e7feb81e4dc2b4b6e62d29c7fab44b",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "48bbb5d859f250ad8f9fc620f98e05f1",
-"index.html": "633934b98bb05310f540ca9ec00e9e38",
-"/": "633934b98bb05310f540ca9ec00e9e38",
-"main.dart.js": "2e46235bb82cc8f9ad082f3718a9fd08",
+"index.html": "6e2cf4eb33b26b6a87e03f34d696232e",
+"/": "6e2cf4eb33b26b6a87e03f34d696232e",
+"main.dart.js": "eba02a63fd8a05425e4b5b3c13105b30",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "039ffd702079a317085c07b25a79dad8",
 "icons/Icon-192.png": "5f011194a98ad88faba3e6bfb6ef74f7",
